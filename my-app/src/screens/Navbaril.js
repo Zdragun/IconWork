84ed3a1a2c16fe4d47c0230fe66f65faf128c652
Zdragun <zdragunnazar@gmail.com>
2022-06-15 17:39:37 +0300

    Added icons

---
+++ my-app/src/screens/Navbaril.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {LogoNodejs} from 'react-ionicons'
+function Navbaril() {
+  return (
+    <div><LogoNodejs height='3em' width='3em' onClick={()=>{console.log('clown')}} color={'#7FFFD4'}/></div>
+  )
+}
+
+export default Navbaril;
